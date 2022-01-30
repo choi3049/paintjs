2408575672b8701b629e2b320500382308b1bc0e
choi3049 <qudtjs3012@naver.com>
2022-01-30 16:49:13 +0900

    Canvas Events

---
+++ app.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+}
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseDown(even) {
+  painting = true;
+}
+
+function onMouseUp(event) {
+  painting = false;
+}
+
+function onMouseLeave(event) {
+  painting = false;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", onMouseLeave);
+}
